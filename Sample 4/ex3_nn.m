ac60c1e3f2949476ab1db33d1c779e9e64f8f857
Tonan <57250494+tonana@users.noreply.github.com>
2023-12-22 15:07:39 +0800

    Add files via upload

---
+++ Sample 4/ex3_nn.m
@@ -0,0 +1,90 @@
+%% 
+%  Assignment 3 - Part 2: Neural Networks
+%
+%  Instructions
+%  ------------
+% 
+%  This file contains code that helps you get started on the assignment.
+%  You will need to complete the following functions in this assignment:
+%
+%     lrCostFunction.m (logistic regression cost function)
+%     oneVsAll.m
+%     predictOneVsAll.m
+%     predict.m
+%
+
+%% Initialization
+clear ; close all; clc
+
+%% Setup parameters for this part
+input_layer_size  = 400;  % 20 x 20 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 1 to 10   
+                          % (note that we have mapped "0" to label 10)
+
+%% =========== Part 1: Loading and Visualizing Data =============
+%  We start the assignment by first loading and visualizing the dataset. 
+%  You will be working with a dataset that contains handwritten digits.
+%
+
+% Load Training Data
+fprintf('Loading and Visualizing Data ...\n')
+
+load('ex3data1.mat');
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+
+displayData(X(sel, :));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ================ Part 2: Loading Parameters ================
+% In this part of the assignment, we load some pre-initialized 
+% neural network parameters.
+
+fprintf('\nLoading Saved Neural Network Parameters ...\n')
+
+% Load the weights into variables Theta1 and Theta2
+load('ex3weights.mat');
+
+%% ================= Part 3: Implement Predict =================
+%  After training the neural network, we would like to use it to predict
+%  the labels. You will now implement the "predict" function to use the
+%  neural network to predict the labels of the training set. This lets
+%  you compute the training set accuracy.
+
+pred = predict(Theta1, Theta2, X);
+
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%  To give you an idea of the network's output, you can also run
+%  through the examples one at a time to see what it is predicting.
+
+%  Randomly permute examples
+rp = randperm(m);
+
+for i = 1:m
+    % Display an image
+    fprintf('\nDisplaying Example Image\n');
+    displayData(X(rp(i), :));
+
+    % Predict the number
+    pred = predict(Theta1, Theta2, X(rp(i),:));
+    fprintf( '\nNeural Network Prediction: %d (digit %d)\n', ...
+		      pred, mod(pred, 10) );
+    
+    % Pause
+    fprintf('Press ENTER to continue or CTRL+C to end.\n');
+    pause;
+end
+
+fprintf('\nProgram paused. Press enter to finish.\n\n');
+pause;
+close all;
